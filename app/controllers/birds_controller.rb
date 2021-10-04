ff43cba647604e3c34d8de8d6a3feb22486abf83
zdurr22 <zdurr22@gmail.com>
2021-10-04 11:42:43 -0400

    commit tree 219171bf79ac6c8de44e16e308c7c3cd0c95f573

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,14 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: birds
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: {id: bird.id, name: bird.name, species: bird.species}
+    else
+      render json: {message: "Bird not found"}
+    end
 end
